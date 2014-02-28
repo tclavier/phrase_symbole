f45fab21dc060e9180c9dcc56046a9dad05d8efd
Thomas Clavier <tclavier@azae.net>
2014-02-28 11:20:32 +0100

    capistranification

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :repository,  "git@gitorious.org:thomas-clavier/codin_gouter_phrase_symbole.
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 set :deploy_to, "/var/www/montessori"
 
-set :user, "deliverous"
+set :user, "montessori"
 set :use_sudo, false
 
 role :web, "vm17.azae.net"                          # Your HTTP server, Apache/etc
@@ -20,10 +20,10 @@ after "deploy:restart", "deploy:cleanup"
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
